448e854e05bcaa5fc456cbfb1515684863ed16d0
John Keiser <jkeiser@opscode.com>
2014-04-12 01:24:33 -0700

    Use current chef server (inherited from parent), transfer creds

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -12,11 +12,6 @@ with_provisioner_options 'bootstrap_options' => {
     'flavor_id' => 'm3.large'
   }
 
-with_chef_server "https://ec2-54-224-52-189.compute-1.amazonaws.com/organizations/chef",  {
-  :client_name => Chef::Config[:node_name],
-  :signing_key_filename => Chef::Config[:client_key]
-}
-
 fog_key_pair "dockerdemo_#{ENV['USER']}" do
   private_key_path "#{ec2testdir}/dockerdemo_#{ENV['USER']}"
   public_key_path "#{ec2testdir}/dockerdemo_#{ENV['USER']}.pub"
--- recipes/provisioner.rb
+++ recipes/provisioner.rb
@@ -1,12 +1,24 @@
-machine 'provisioner' do
-  file '/etc/chef/validation.pem', "#{ENV['HOME']}/.chef/chef-validator.pem"
-  recipe 'apt'
-  recipe 'build-essential'
-  #recipe 'git'
-end
+with_provisioner_options({})
+
+ChefMetal.enclosing_provisioner.compute_options[:ssh_username] = 'ubuntu'
 
 machine 'provisioner' do
-  #recipe 'docker'
+  recipe 'apt'
+  recipe 'build-essential'
+end
+
+credentials = ChefMetal::AWSCredentials.new
+credentials.load_default
+aws_config = <<EOM
+[default]
+aws_access_key_id=#{credentials['jkeiser'][:access_key_id]}
+aws_secret_access_key=#{credentials['jkeiser'][:secret_access_key]}
+region=#{credentials['jkeiser'][:region]}
+EOM
+
+machine 'provisioner' do
+  file '/home/ubuntu/.aws/config', :content => aws_config
+
   recipe 'docker-demo::install_metal'
   recipe 'docker-demo::ec2'
   recipe 'docker-demo::mongo_cluster'
