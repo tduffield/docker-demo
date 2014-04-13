7eba3d08cb3bf3a822f6fd180a313660dc178993
John Keiser <jkeiser@opscode.com>
2014-04-12 19:43:52 -0700

    Use globally recognizable ssh key, universal ami

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -4,11 +4,6 @@ ec2testdir = "#{ENV['HOME']}/ec2creds"
 
 directory ec2testdir
 
-with_chef_server "https://manage-chefconf-demo.opscode.com/organizations/chef", {
-  :client_name => Chef::Config[:node_name],
-  :signing_key_filename => Chef::Config[:client_key]
-}
-
 with_fog_ec2_provisioner :ssh_username => 'ec2-user'
 
 with_provisioner_options 'bootstrap_options' => {
@@ -17,7 +12,7 @@ with_provisioner_options 'bootstrap_options' => {
     'block_device_mapping' => [{'DeviceName' => '/dev/sda1', 'Ebs.VolumeSize' => 30}]
   }
 
-fog_key_pair "dockerdemo_#{ENV['USER']}" do
-  private_key_path "#{ec2testdir}/dockerdemo_#{ENV['USER']}"
-  public_key_path "#{ec2testdir}/dockerdemo_#{ENV['USER']}.pub"
+fog_key_pair "dockerdemo_ssh_key" do
+  private_key_path "#{ec2testdir}/dockerdemo_ssh_key"
+  public_key_path "#{ec2testdir}/dockerdemo_ssh_key.pub"
 end
