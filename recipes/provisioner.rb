2c970d25843ab2167fce2d7d84e3e8ff25b45fd1
John Keiser <jkeiser@opscode.com>
2014-04-12 19:45:16 -0700

    Use default credentials, upload ssh key, make build-essential work

--- recipes/provisioner.rb
+++ recipes/provisioner.rb
@@ -1,25 +1,22 @@
-with_provisioner_options({})
-
-ChefMetal.enclosing_provisioner.compute_options[:ssh_username] = 'ubuntu'
-
-machine 'provisioner' do
-  recipe 'apt'
-  recipe 'build-essential'
-end
-
 credentials = ChefMetal::AWSCredentials.new
 credentials.load_default
 aws_config = <<EOM
 [default]
-aws_access_key_id=#{credentials['jkeiser'][:access_key_id]}
-aws_secret_access_key=#{credentials['jkeiser'][:secret_access_key]}
-region=#{credentials['jkeiser'][:region]}
+aws_access_key_id=#{credentials['default'][:access_key_id]}
+aws_secret_access_key=#{credentials['default'][:secret_access_key]}
+region=#{credentials['default'][:region]}
 EOM
 
 machine 'provisioner' do
   file '/home/ubuntu/.aws/config', :content => aws_config
+  file '/home/ubuntu/ec2creds/dockerdemo_ssh_key', "#{ENV['HOME']}/ec2creds/dockerdemo_ssh_key"
 
+  attribute %w(build-essential compile_time), true
+
+  recipe 'apt'
+  recipe 'build-essential'
   recipe 'docker-demo::install_metal'
   recipe 'docker-demo::ec2'
   recipe 'docker-demo::mongo_cluster'
+  converge true
 end
